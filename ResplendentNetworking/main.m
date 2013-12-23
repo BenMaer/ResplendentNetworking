a7c6499bf3a1b999e81330a8dc494d610957f715
BenMaer <ben@resplendent.co>
2013-12-23 17:15:00 -0500

    adding networking

---
+++ ResplendentNetworking/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ResplendentNetworking
+//
+//  Created by Benjamin Maer on 12/23/13.
+//  Copyright (c) 2013 Resplendent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RNAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RNAppDelegate class]));
+    }
+}
